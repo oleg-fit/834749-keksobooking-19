d9956c4f2a423f9b2d8e5003e48f2b7b01b91969
oleg-fit <oleg26-spb@rambler.ru>
2020-01-26 18:09:33 +0300

    Добавляет правки

--- js/main.js
+++ js/main.js
@@ -8,31 +8,42 @@ var guests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var checkin = ['12:00', '13:00', '14:00'];
 var checkout = ['12:00', '13:00', '14:00'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-// // var description
-var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg', 'http://o0.github.io/assets/images/tokyo/hotel4.jpg', 'http://o0.github.io/assets/images/tokyo/hotel5.jpg'];
-
 
 var choosingRandomValue = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-var choosingRandomFeatures = function (features) {
+var choosingRandomFeatures = function (arr2) {
+  var i = 0;
+  var k = 0;
   var arr = [];
+  var maxArrLenght = 1 + Math.floor(Math.random() * (arr2.length - 1));
 
-  for (var i = 0; i < Math.ceil(Math.random() * features.length); i++) {
+  while (i < maxArrLenght) {
+    var randomElementFeatures = arr2[Math.floor(Math.random() * arr2.length)];
 
-    arr.push(features[Math.floor(Math.random() * features.length)]);
+    while (k < arr.length) {
+      if (arr[k] !== randomElementFeatures) {
+        arr.push(randomElementFeatures);
+      }
+      k++;
+    }
+    i++;
   }
+
   return arr;
 };
 
-console.log(choosingRandomFeatures(features));
+function randomInteger(min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
 
-// var author = {
-  // avatar: 'img/avatars/user.png'
-  // не сделано
-  // адрес изображения вида img/avatars/user**{{xx}}**.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-// };
+// console.log(choosingRandomFeatures(features));
+
+var author = {
+  avatar: 'img/avatars/user0' + randomInteger(1, 8) + '.png'
+};
 
 var offer = {
   title: choosingRandomValue(title),
@@ -44,14 +55,11 @@ var offer = {
   checkin: choosingRandomValue(checkin),
   checkout: choosingRandomValue(checkout),
   features: choosingRandomFeatures(features)
-  // description: строка с описанием,
-  // photos: массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
 };
 
 // var location = {
-  // x: 100,
-  // случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-  // y: 135
-  // случайное число, координата y метки на карте от 130 до 630.
+//   x: 100,
+//   y: randomInteger(130, 630)
 // }
+
 

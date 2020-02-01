7481c4f0e445c5752178b0c41e580ff582212664
oleg-fit <oleg26-spb@rambler.ru>
2020-02-01 18:38:26 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 'use strict';
 
 var TITLE = 'Заголовок';
-var MAXPRICE = 6000;
+var MAXPRICE = 1000000;
 var MAXROOMS = 5;
 var MAXGUESTS = 10;
 var DESCRIPTION = 'Описание';
-
-var map = document.querySelector('.map');
-var mapWidth = map.offsetWidth;
-var minLocationXMap = map.offsetLeft;
-var maxLocationXMap = map.offsetLeft + mapWidth;
 
 var type = [
   'palace',
@@ -72,9 +67,20 @@ var author = {
   avatar: 'img/avatars/user0' + randomInteger(1, 8) + '.png' // сделано
 };
 
+var map = document.querySelector('.map');
+var mapWidth = map.offsetWidth;
+var minLocationXMap = map.offsetLeft;
+var maxLocationXMap = map.offsetLeft + mapWidth;
+
+// все исчезает с кодом нижу
+// var location = {
+//   x: randomInteger(minLocationXMap, maxLocationXMap),
+//   y: randomInteger(130, 630)
+// };
+
 var offer = {
   title: TITLE, // сделано
-  // address: 'location.x, location.y', // не сделано
+  address: location.x + ', ' + location.y, // не сделано
   price: randomInteger(0, MAXPRICE), // сделано
   type: choosingRandomValue(type), // сделано
   rooms: randomInteger(1, MAXROOMS), // сделано
@@ -86,11 +92,55 @@ var offer = {
   photos: choosingRandomArr(photos) // сделано
 };
 
-// все исчезает с кодом нижу
-// var location = {
-//   x: randomInteger(minLocationXMap, maxLocationXMap),
-//   y: randomInteger(130, 630)
-// };
+
 
 map.classList.remove('map--faded');
 
+var mapPins = document.querySelector('.map__pins');
+var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
+
+for (var i = 0; i < 8; i++) {
+  var pinElement = templatePin.cloneNode(true);
+
+  // var pinElementWidth = pinElement.offsetWidth;
+  // var pinElementHeight = pinElement.offsetHeight;
+  // console.log(pinElementWidth);
+  // console.log(pinElementHeight);
+
+
+  pinElement.style.left = randomInteger(minLocationXMap, maxLocationXMap) + 'px';
+  pinElement.style.top = randomInteger(130, 630) + 'px';
+  pinElement.querySelector('img').src = 'img/avatars/user0' + randomInteger(1, 8) + '.png';
+  pinElement.querySelector('img').alt = offer.title;
+
+  mapPins.appendChild(pinElement);
+}
+
+
+
+
+
+
+// var renderPin = function() {
+//   var pinElement = templatePin.cloneNode(true);
+
+  // var pinElementWidth = pinElement.offsetWidth;
+  // var pinElementHeight = pinElement.offsetHeight;
+  // console.log(pinElementWidth);
+  // console.log(pinElementHeight);
+
+
+//   pinElement.style.left = randomInteger(minLocationXMap, maxLocationXMap) + 'px';
+//   pinElement.style.top = randomInteger(130, 630) + 'px';
+//   pinElement.querySelector('img').src = 'img/avatars/user0' + randomInteger(1, 7) + '.png';
+//   pinElement.querySelector('img').alt = offer.title;
+
+//   return pinElement;
+// };
+
+// var fragment = document.createDocumentFragment();
+// for (var i = 0; i < 8; i++) {
+//   fragment.appendChild(renderPin(pinElement[i]));
+// }
+
+// similarListElement.appendChild(fragment);

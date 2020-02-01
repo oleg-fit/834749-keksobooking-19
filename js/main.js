498268b3474d5de4742af90109e1439e8ea965c7
oleg-fit <oleg26-spb@rambler.ru>
2020-02-01 13:48:36 +0300

    Добавляет правки по пункту один

--- js/main.js
+++ js/main.js
@@ -1,65 +1,96 @@
 'use strict';
 
-var title = ['Уютная', 'Комфортабельная', 'Крохотная', 'Пустая', 'Светлая'];
-var price = [1000, 2000, 3000, 4000, 5000, 6000];
-var type = ['palace', 'flat', 'house', 'bungalo'];
-var rooms = [1, 2, 3, 4, 5];
-var guests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var checkin = ['12:00', '13:00', '14:00'];
-var checkout = ['12:00', '13:00', '14:00'];
-var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var TITLE = 'Заголовок';
+var MAXPRICE = 6000;
+var MAXROOMS = 5;
+var MAXGUESTS = 10;
+var DESCRIPTION = 'Описание';
+
+var map = document.querySelector('.map');
+var mapWidth = map.offsetWidth;
+var minLocationXMap = map.offsetLeft;
+var maxLocationXMap = map.offsetLeft + mapWidth;
+
+var type = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+
+var checkin = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+var checkout = [
+  '12:00',
+  '13:00',
+  '14:00'];
+
+var features = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+var photos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
+function randomInteger(min, max) {
+  var rand = min + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
 
 var choosingRandomValue = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-var choosingRandomFeatures = function (arr2) {
-  var i = 0;
-  var k = 0;
-  var arr = [];
-  var maxArrLenght = 1 + Math.floor(Math.random() * (arr2.length - 1));
-
-  while (i < maxArrLenght) {
-    var randomElementFeatures = arr2[Math.floor(Math.random() * arr2.length)];
-
-    while (k < arr.length) {
-      if (arr[k] !== randomElementFeatures) {
-        arr.push(randomElementFeatures);
-      }
-      k++;
+function choosingRandomArr (arr) {
+  var arrQuantity = 1 + Math.floor(Math.random() * (arr.length - 1));
+  var arrPart = [];
+  var arrIndex = 0;
+  var usedIndexes = {};
+  while (arrPart.length < arrQuantity) {
+    arrIndex = Math.floor(Math.random() * arr.length);
+    if (usedIndexes[arrIndex] === undefined) {
+      usedIndexes[arrIndex] = true;
+      arrPart.push(arr[arrIndex]);
     }
-    i++;
   }
-
-  return arr;
-};
-
-function randomInteger(min, max) {
-  var rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.round(rand);
+  return arrPart;
 }
 
-// console.log(choosingRandomFeatures(features));
-
 var author = {
-  avatar: 'img/avatars/user0' + randomInteger(1, 8) + '.png'
+  avatar: 'img/avatars/user0' + randomInteger(1, 8) + '.png' // сделано
 };
 
 var offer = {
-  title: choosingRandomValue(title),
-  address: '600, 350',
-  price: choosingRandomValue(price),
-  type: choosingRandomValue(type),
-  rooms: choosingRandomValue(rooms),
-  guests: choosingRandomValue(guests),
-  checkin: choosingRandomValue(checkin),
-  checkout: choosingRandomValue(checkout),
-  features: choosingRandomFeatures(features)
+  title: TITLE, // сделано
+  // address: 'location.x, location.y', // не сделано
+  price: randomInteger(0, MAXPRICE), // сделано
+  type: choosingRandomValue(type), // сделано
+  rooms: randomInteger(1, MAXROOMS), // сделано
+  guests: randomInteger(0, MAXGUESTS), // сделано
+  checkin: choosingRandomValue(checkin), // сделано
+  checkout: choosingRandomValue(checkout), // сделано
+  features: choosingRandomArr(features), // сделано
+  description: DESCRIPTION, // сделано
+  photos: choosingRandomArr(photos) // сделано
 };
 
+// все исчезает с кодом нижу
 // var location = {
-//   x: 100,
+//   x: randomInteger(minLocationXMap, maxLocationXMap),
 //   y: randomInteger(130, 630)
-// }
+// };
 
+map.classList.remove('map--faded');
 

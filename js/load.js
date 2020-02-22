95abd537743f81eb3586a5958ed469605abcc31e
oleg-fit <oleg26-spb@rambler.ru>
2020-02-22 16:55:38 +0300

    Правки

--- js/load.js
+++ js/load.js
@@ -1,74 +1,64 @@
 'use strict';
 
 (function () {
-  var URL_DATA = 'https://js.dump.academy/keksobooking/data';
-
-  var STATUS = {
-    CODE_OK: 200
-  };
-
   var TIMEOUT = 10000;
 
-  var load = function (onSuccess, onError) {
+  var STATUS = {
+    OK: 200,
+  };
+
+  var URL_DATA = 'https://js.dump.academy/keksobooking/data';
+  var URL_SAVE = 'https://js.dump.academy/keksobooking';
+
+  var initXhr = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
+
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === STATUS.CODE_OK) {
+      if (xhr.status === STATUS.OK) {
         onSuccess(xhr.response);
       } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        onError('Status:' + xhr.response);
       }
     });
 
     xhr.addEventListener('error', function () {
-      onError('Проверьте интернет соединение');
+      onError('XHR error');
     });
 
     xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      onError('XHR timeout' + xhr.timeout);
     });
 
     xhr.timeout = TIMEOUT;
 
-    xhr.open('GET', URL_DATA.load);
+    return xhr;
+  };
+
+  var getData = function (onSuccess, onError) {
+    var xhr = initXhr(onSuccess, onError);
+    xhr.open('GET', URL_DATA);
     xhr.send();
   };
 
-  var similarCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
-
-  var renderCards = function (popapCard) {
-    var popupCardElement = similarCardTemplate.cloneNode(true);
-
-    // Добаляем заголовок на карту объявления
-    popupCardElement.querySelector('.popup__title').textContent = popapCard.offer.title;
-
-    // Добаляем координаты метки на карту объявления
-    popupCardElement.querySelector('.popup__text--address').textContent = popapCard.offer.address;
-
-    // Добаляем цену за ночь на карту на карту объявления
-    popupCardElement.querySelector('.popup__text--price').textContent = popapCard.offer.price + 'P/ночь';
-
-    return popupCardElement;
+  var saveData = function (onSuccess, onError, data) {
+    var xhr = initXhr(onSuccess, onError);
+    xhr.open('POST', URL_SAVE);
+    xhr.send(data);
   };
 
-  var MAX_ADVERTISEMENT_QUANTITY = 8;
+  var objectsArray = [];
 
-  var successHandler = function (popapCards) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < MAX_ADVERTISEMENT_QUANTITY; i++) {
-      fragment.appendChild(renderCards(popapCards[i]));
-    }
-
-    // similarListElement.appendChild(fragment);
-
+  var onSuccess = function (data) {
+    objectsArray = data;
+    return objectsArray;
   };
 
-  window.data = {
-    successHandler: successHandler,
-    load: load
+  window.request = {
+    getData: getData,
+    saveData: saveData,
+    onSuccess: onSuccess
   };
-
 
 })();
